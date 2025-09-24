72fce0b318d43a45f601f7b9ea023e1b4a9bec4a
Victor Chicu <victor.chicu@hotmail.com>
2025-09-24 22:25:45 +0300

    Run mongodb as replica set

--- mongo-init/01-init.js
+++
@@ -1,6 +0,0 @@
-rs.initiate({_id: "rs0", members: [{ _id: 0, host: "mongo:27017" }]});
-db.getSiblingDB("admin").createUser({
-    user: _getEnv("MONGO_INITDB_ROOT_USERNAME") || "admin",
-    pwd:  _getEnv("MONGO_INITDB_ROOT_PASSWORD") || "admin",
-    roles: [{ role: "root", db: "admin" }]
-});
