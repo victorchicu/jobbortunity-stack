74394b1fc3c33114c8fa286b5b3f66647dc55cc2
Victor Chicu <victor@aroundwire.com>
2025-09-25 11:36:44 +0300

    Prefix {project name} to back up files.

--- backup.sh
+++ backup.sh
@@ -4,12 +4,14 @@ OUT=${1:-$HOME/Desktop/jobbortunity-backup.tar.gz}
 TMP=$(mktemp -d)
 mkdir -p "$TMP"
 
-docker run --rm -v mongo_data:/data -v "$TMP":/backup busybox \
-  tar czf /backup/mongo_data.tar.gz -C /data .
-docker run --rm -v rabbitmq_data:/data -v "$TMP":/backup busybox \
-  tar czf /backup/rabbitmq_data.tar.gz -C /data .
-docker run --rm -v rabbitmq_log:/data -v "$TMP":/backup busybox \
-  tar czf /backup/rabbitmq_log.tar.gz -C /data .
+docker run --rm -v jobbortunity-stack_mongo_data:/data -v "$TMP":/backup busybox \
+  tar czf /backup/jobbortunity-stack_mongo_data.tar.gz -C /data .
+
+docker run --rm -v jobbortunity-stack_rabbitmq_data:/data -v "$TMP":/backup busybox \
+  tar czf /backup/jobbortunity-stack_rabbitmq_data.tar.gz -C /data .
+
+docker run --rm -v jobbortunity-stack_rabbitmq_log:/data -v "$TMP":/backup busybox \
+  tar czf /backup/jobbortunity-stack_rabbitmq_log.tar.gz -C /data .
 
 tar czf "$OUT" -C "$TMP" .
 echo "Backup $OUT"
